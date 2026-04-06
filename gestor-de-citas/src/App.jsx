fc0af29af81446040a20639c6f77d53423d2a495
Maxi Bernardoni <maxibernardoni@gmail.com>
2026-04-06 13:48:03 -0300

    Errores solucionados

--- gestor-de-citas/src/App.jsx
+++ gestor-de-citas/src/App.jsx
@@ -5,47 +5,47 @@ import heroImg from './assets/hero.png'
 import React from "react";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-  function Titulo(props) {
+function Titulo(props) {
     return (
       <h2>{props.titulo}</h2>
     )
-  }
-  function ListadoCitas() {
+}
+function ListadoCitas() {
     return (
-    <div className="lista-citas">
+    <>
       <Cita nombre="Niña" dueño="Martin" fecha="2021-08-05" hora="08:20" sintomas="Le duele la pierna"/>
       <Cita nombre="Sifon" dueño="Flecha" fecha="2023-08-05" hora="09:24" sintomas="Duerme mucho"/>
       <Cita nombre="Floki" dueño="Ari" fecha="2023-08-05" hora="16:15" sintomas="No está comiendo"/>
-    </div>
+    </>
     );
-  }
-  function Cita(props) {
+}
+function Cita(props) {
     return(
-    <div className="cita">
+    <div className="Cita">
               <p>Mascota: <span>{props.nombre}</span></p>
               <p>Dueño: <span>{props.dueño}</span></p>
               <p>Fecha: <span>{props.fecha}</span></p>
               <p>Hora: <span>{props.hora}</span></p>
               <p>Sintomas: <span>{props.sintomas}</span></p><button className="button u-full-width">Eliminar ×</button>
-            </div>)
-  }
-  function Formulario() {
+    </div>)
+}
+function Formulario() {
     return (
       <>
-
-            <h2>Crear mi Cita</h2>
-            <form><label>Nombre Mascota</label><input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" value="" readOnly/><label>
+      <Titulo titulo="Crear mi Cita" />
+        <form><label>Nombre Mascota</label><input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" value="" readOnly/><label>
                      Nombre Dueño</label><input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" value="" readOnly/><label>
                       Fecha</label><input type="date" name="fecha" className="u-full-width" value="" readOnly/><label>
                       Hora</label><input type="time" name="hora" className="u-full-width" value="" readOnly/><label>Sintomas</label><textarea name="sintomas" className="u-full-width" readOnly></textarea>
                      <button type="submit"
                      className="u-full-width button-primary">Agregar Cita</button></form>
 
-          </>
+        </>
     )
-  }
+}
+function App() {
+  const [count, setCount] = useState(0)
+  
   return (
     <>
       <section id="center">
